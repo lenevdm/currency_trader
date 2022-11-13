98f6e0a4e3c7013a189e6140db9e2daafc047acf
Lené van der Merwe <lenebooysen@gmail.com>
2022-11-13 14:20:33 +0200

    W3: bug fixes

--- main.cpp
+++ main.cpp
@@ -4,151 +4,126 @@
 
 enum class OrderBookType{bid, ask};
 
-class OrderBookEntry {
+class OrderBookEntry
+{
     public:
         double price;
         double amount;
         std::string timestamp;
         std::string product;
-        OrderBookType orderType;
+        OrderBookType orderType; 
+
 };
 
 void printMenu()
 {
-    // 1 Print help
-    std::cout << "1. Print help " << std::endl;
-    // 2 Print exchange stats
-    std::cout << "2. Print exchange stats " << std::endl;
-    // 3 Make an offer
-    std::cout << "3. Make an offer " << std::endl;
-    // 4 Make a bid
-    std::cout << "4. Make a bid " << std::endl;
-    // 5 Print wallet
-    std::cout << "5. Print wallet " << std::endl;
-    // 6 Continue
-    std::cout << "6. Continue " << std::endl;
+    // 1 print help
+    std::cout << "1: Print help " << std::endl;
+    // 2 print exchange stats
+    std::cout << "2: Print exchange stats" << std::endl;
+    // 3 make an offer
+    std::cout << "3: Make an offer " << std::endl;
+    // 4 make a bid 
+    std::cout << "4: Make a bid " << std::endl;
+    // 5 print wallet
+    std::cout << "5: Print wallet " << std::endl;
+    // 6 continue   
+    std::cout << "6: Continue " << std::endl;
 
-    
-    std::cout << "===================" << std::endl;
-    std::cout << "Type in 1-6." << std::endl;
-    
+    std::cout << "============== " << std::endl;
 }
 
-// Menu functions
 void printHelp()
 {
-    std::cout << "Your aim is to make money, analyze the market and make bids and offers." << std::endl;
+    std::cout << "Help - your aim is to make money. Analyse the market and make bids and offers. " << std::endl;
 }
 
-void printMarketStats()
+void  printMarketStats()
 {
-    std::cout << "Market looks good." << std::endl;
+    std::cout << "Market looks good. " << std::endl;
 }
 
 void enterOffer()
 {
-    std::cout << "Make an offer - enter the amount." << std::endl;
+    std::cout << "Mark and offer - enter the amount " << std::endl;
 }
 
 void enterBid()
 {
-    std::cout << "Make a bid - enter the amount." << std::endl;
+    std::cout << "Make a bid - enter the amount" << std::endl;
 }
 
 void printWallet()
 {
-    std::cout << "Your wallet is empty." << std::endl;
+    std::cout << "Your wallet is empty. " << std::endl;
 }
-
-void gotoNextTimeFrame()
+        
+void gotoNextTimeframe()
 {
-    std::cout << "Going to next timeframe." << std::endl;
+    std::cout << "Going to next time frame. " << std::endl;
 }
-
+ 
 int getUserOption()
 {
     int userOption;
+
+    std::cout << "Type in 1-6" << std::endl;
     std::cin >> userOption;
-    std::cout << "You chose: " <<  userOption << std::endl;
+    std::cout << "You chose: " << userOption << std::endl;
     return userOption;
 }
 
 void processUserOption(int userOption)
 {
-    if (userOption == 0) // Bad input
+    if (userOption == 0) // bad input
     {
-        std::cout << "Invalid choice. Choose 1-6." << std::endl;
+        std::cout << "Invalid choice. Choose 1-6" << std::endl;
     }
-    if (userOption == 1) // 1 Print help
+    if (userOption == 1) // bad input
     {
         printHelp();
     }
-    if (userOption == 2) // 2 Print exchange stats
+    if (userOption == 2) // bad input
     {
         printMarketStats();
     }
-    if (userOption == 3) // 3 Make an offer
+    if (userOption == 3) // bad input
     {
         enterOffer();
     }
-    if (userOption == 4) // 4 Make a bid
+    if (userOption == 4) // bad input
     {
         enterBid();
     }
-    if (userOption == 5) // 5 Print wallet
+    if (userOption == 5) // bad input
     {
         printWallet();
     }
-    if (userOption == 6) // 6 Continue
+    if (userOption == 6) // bad input
     {
-        gotoNextTimeFrame();
-    }
+        gotoNextTimeframe();
+    }       
 }
 
 int main()
-{
-//     //Representing multiple order book entries using vectors
-//     std::vector<double> prices;
-//     std::vector<double> amounts;
-//     std::vector<std::string> timestamps;
-//     std::vector<std::string> products;
-//     std::vector<OrderBookType> orderTypes;
-    
-//     //Add data to vectors using push_back
-//     prices.push_back(500.01);
-//     amounts.push_back(0.001);
-//     timestamps.push_back("2020/03/17 17:01:24.884492");
-//     products.push_back("BTC/USDT");
-//     orderTypes.push_back(OrderBookType::bid);
+{    
+    // while (true)
+    // {
+    //     printMenu();
+    //     int userOption = getUserOption();
+    //     processUserOption(userOption);
+    // }
+    // return 0;
 
-//     prices.push_back(1000.01);
-//     amounts.push_back(0.001);
-//     timestamps.push_back("2020/03/17 17:01:24.88449");
-//     products.push_back("BTC/USDT");
-//     orderTypes.push_back(OrderBookType::ask);
-
-//     //Accessing data in the vectors
-//     std::cout <<"prices: " << prices[0] << std::endl;
-//     std::cout <<"prices: " << prices[1] << std::endl;
-
-//     // while(true)
-//     // {
-//     //     printMenu();
-
-//     //     int userOption = getUserOption();
-
-//     //     processUserOption(userOption);
-//     // }
-    
-//     return 0;  
-
-    OrderBookEntry order1; //create an instance of the class
+    OrderBookEntry order1;
     order1.price = 10000;
     order1.amount = 0.002;
-    order1.timestamp = "2020/03/17 17:01:24.884492";
+    order1.timestamp = "17:01:24.884492";
     order1.product = "BTC/USDT";
     order1.orderType = OrderBookType::bid;
 
     std::cout << "The price is " << order1.price << std::endl;
 
+
 }
+
